d5de2acc58e947b7109bdee1ae4f2a9d55b56d40
Rory Hellier <rory.hellier@googlemail.com>
2021-02-11 22:02:09 +0000

    Enables Full Responsive Website Reaction Across All Screen Sizes:
     - Enables resizing of the two navbar components and their contents
     - Enables Resizing of all fonts on the homepage depending on screen sizes
     - Reorders and enables up to 4 product list items (on 1200px+ screens) on the homepage
     - Properly resizes the categoryFilters component contents on screen resize
     - Fixes bug where categoryFilterInput dropdown would float off screen
     - Adds mulitple productImageComponent returns with different SwiperJS settings depeding on screen size
     - Switches the imageContainer on the productPage from being fixed height to auto, enabling the info container to stick to the bottom of the image

--- src/pages/ProductPage/components/ProductImageComponent/ProductImageComponent.jsx
+++ src/pages/ProductPage/components/ProductImageComponent/ProductImageComponent.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/no-array-index-key */
@@ -12,22 +13,89 @@ import styles from './ProductImageComponent.module.scss';
 
 SwiperCore.use([Pagination, Scrollbar, A11y]);
 
-const ProductImageComponent = ({ images, brandName, type }) => (
-  <Swiper
-    spaceBetween={50}
-    slidesPerView={1}
-    pagination
-    className={styles.swiperGallery}
-  >
-    {
-      images.map((image, index) => (
-        <SwiperSlide className={styles.swiperGallerySlide} key={`${brandName}-${index}`}>
-          <img className={styles.swiperGalleryImage} src={image} alt={`${brandName} ${type}`} />
-        </SwiperSlide>
-      ))
-    }
-  </Swiper>
-);
+const ProductImageComponent = ({ images, brandName, type }) => {
+  const mqxs = window.matchMedia('(max-width: 468px)');
+  const mqs = window.matchMedia('(max-width: 768px)');
+  const mqm = window.matchMedia('(max-width: 1024px)');
+  const mql = window.matchMedia('(max-width: 1200px)');
+
+  return mqxs.matches ? (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      pagination
+      className={styles.swiperGallery}
+    >
+      {
+        images.map((image, index) => (
+          <SwiperSlide className={styles.swiperGallerySlide} key={`${brandName}-${index}`}>
+            <img className={styles.swiperGalleryImage} src={image} alt={`${brandName} ${type}`} />
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+  ) : mqs.matches ? (
+    <Swiper
+      spaceBetween={10}
+      slidesPerView={2}
+      pagination
+      className={styles.swiperGallery}
+    >
+      {
+        images.map((image, index) => (
+          <SwiperSlide className={styles.swiperGallerySlide} key={`${brandName}-${index}`}>
+            <img className={styles.swiperGalleryImage} src={image} alt={`${brandName} ${type}`} />
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+  ) : mqm.matches ? (
+    <Swiper
+      spaceBetween={10}
+      slidesPerView={2}
+      pagination
+      className={styles.swiperGallery}
+    >
+      {
+        images.map((image, index) => (
+          <SwiperSlide className={styles.swiperGallerySlide} key={`${brandName}-${index}`}>
+            <img className={styles.swiperGalleryImage} src={image} alt={`${brandName} ${type}`} />
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+  ) : mql.matches ? (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={2}
+      pagination
+      className={styles.swiperGallery}
+    >
+      {
+        images.map((image, index) => (
+          <SwiperSlide className={styles.swiperGallerySlide} key={`${brandName}-${index}`}>
+            <img className={styles.swiperGalleryImage} src={image} alt={`${brandName} ${type}`} />
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+  ) : (
+    <Swiper
+      spaceBetween={10}
+      slidesPerView={3}
+      pagination
+      className={styles.swiperGallery}
+    >
+      {
+        images.map((image, index) => (
+          <SwiperSlide className={styles.swiperGallerySlide} key={`${brandName}-${index}`}>
+            <img className={styles.swiperGalleryImage} src={image} alt={`${brandName} ${type}`} />
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+  );
+};
 
 ProductImageComponent.propTypes = {
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
